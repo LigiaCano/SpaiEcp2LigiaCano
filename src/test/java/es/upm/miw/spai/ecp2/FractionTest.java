a50bb25a1653321bba140709e9e6dc551f9b32a2
Adan <Adan@Adan-PC>
2015-10-03 16:34:09 +0200

    Creacion Test Fraction

---
+++ src/test/java/es/upm/miw/spai/ecp2/FractionTest.java
@@ -0,0 +1,43 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FractionTest {
+	private Fraction fr;
+	
+	 @Before
+	    public void before() {
+	        fr = new Fraction(1, 2);
+	    }
+	 
+	@Test
+	public void testFractionIntInt() {
+		 assertEquals(1, fr.getNumerator());
+	     assertEquals(2, fr.getDenominator());
+	}
+
+	@Test
+	public void testFraction() {
+		assertEquals(1,1);
+	}
+
+	@Test
+	public void testGetNumerator() {
+		assertEquals(1, fr.getNumerator());
+	}
+
+	@Test
+	public void testGetDenominator() {
+		assertEquals(2, fr.getDenominator());
+	}
+
+	@Test
+	public void testDecimal() {
+		assertEquals(0.5, fr.decimal(),10e-5);
+	}
+
+}
