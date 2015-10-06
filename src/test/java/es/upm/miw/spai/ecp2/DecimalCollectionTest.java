750470c3a86c8ba6fd4bd2a8834597fde036decb
Mona <monashh_m@hotmail.com>
2015-10-07 00:27:09 +0200

    Creado test testLower en la clase DecimalCollectionTest

--- src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -5,42 +5,46 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class DecimalCollectionTest {
 	private DecimalCollection dc;
-	
+
 	@Before
-    public void before() {
+	public void before() {
 		dc = new DecimalCollection();
-        
+
 		dc.add(3.0);
 		dc.add(2.5);
 		dc.add(1.3);
-    }
+	}
 
 	@Test
 	public void testDecimalCollection() {
-		assertEquals(3.0,2.5,1.3);
+		assertEquals(3.0, 2.5, 1.3);
 	}
 
 	@Test
 	public void testSize() {
-		  assertEquals(3, dc.size()); 
+		assertEquals(3, dc.size());
 	}
 
 	@Test
 	public void testSum() {
-		assertEquals(6.8, dc.sum(),0);
+		assertEquals(6.8, dc.sum(), 0);
 	}
 
 	@Test
 	public void testHigher() {
-		assertEquals(3.0, dc.higher(),0); 
+		assertEquals(3.0, dc.higher(), 0);
 	}
-	
+
 	@Test
-    public void testAverage() {
-        assertEquals(2.26666667, dc.average(), 10e-5);
-    }
+	public void testAverage() {
+		assertEquals(2.26666667, dc.average(), 10e-5);
+	}
+
+	@Test
+	public void testLower() {
+		assertEquals(1.3, dc.lower(), 0);
+	}
 
 }
