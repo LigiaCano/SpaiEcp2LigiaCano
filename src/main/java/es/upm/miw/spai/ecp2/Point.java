b8c7cd611f9870f0d8e2cd51a511373a595acb4d
Mona <monashh_m@hotmail.com>
2015-10-07 06:43:22 +0200

    Creado método sum en la clase Point

--- src/main/java/es/upm/miw/spai/ecp2/Point.java
+++ src/main/java/es/upm/miw/spai/ecp2/Point.java
@@ -1,53 +1,59 @@
 package es.upm.miw.spai.ecp2;
 
 public class Point {
-    private int x, y;
+	private int x, y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    public Point(int xy) {
-        this(xy, xy);
-    }
+	public Point(int xy) {
+		this(xy, xy);
+	}
 
-    public Point() {
-        this(0, 0);
-    }
+	public Point() {
+		this(0, 0);
+	}
 
-    public int getX() {
-        return this.x;
-    }
+	public int getX() {
+		return this.x;
+	}
 
-    public int getY() {
-        return this.y;
-    }
-    
-    public void setX(int x){
-    	this.x = x;
-    }
-    
-    public void setY(int y){
-    	this.y = y;
-    }
+	public int getY() {
+		return this.y;
+	}
 
-    public double module() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
+	public void setX(int x) {
+		this.x = x;
+	}
 
-    public double phase() {
-        return Math.atan((double) this.y / this.x);
-    }
+	public void setY(int y) {
+		this.y = y;
+	}
 
-    public void translateOrigin(Point origin) {
-        this.x -= origin.getX();
-        this.y -= origin.getY();
-    }
-    
-    @Override
-    public String toString() {
-        return "Point[" + x + "," + y + "]";
-    }
-    
+	public double module() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+
+	public double phase() {
+		return Math.atan((double) this.y / this.x);
+	}
+
+	public void translateOrigin(Point origin) {
+		this.x -= origin.getX();
+		this.y -= origin.getY();
+	}
+
+	public Point sum(Point b) {
+		this.x += b.getX();
+		this.y += b.getY();
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "Point[" + x + "," + y + "]";
+	}
+
 }
